f022d68d1fb8d6ec812f8feea58ec82befeded1a
Marcin Czarkowski <marcinczarkowski@onet.eu>
2018-02-18 15:21:43 +0100

    Create Coin component prototype

---
+++ src/components/CoinList/Coin/Coin.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Coin = () => {
+  return (
+    <div>
+      <p>Bitcoin</p>
+      <ul>
+        <li className="curreny-acronym">Actronym: BTC</li>
+        <li className="currency-value">Current value: 8.800 $</li>
+        <li className="currency-cap">Market cap: 2,3 mld</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Coin;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+
+import Coin from "../components/CoinList/Coin/Coin";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Coin />
       </div>
     );
   }
