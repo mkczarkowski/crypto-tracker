ed60bc6499f816ed1275eee8626138804bcfbc32
Marcin Czarkowski <marcinczarkowski@onet.eu>
2018-03-04 09:40:47 +0100

    Display market cap data dynamically

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-const Header = () => (
+import PropTypes from "prop-types";
+
+const Header = ({ cap }) => (
   <div>
     <h1>Crypto Tracker</h1>
-    <p>Market Cap: $497 070 174 372</p>
+    <p>
+      Market Cap: ${String(cap)
+        .replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+        .trim()}
+    </p>
   </div>
 );
+
+Header.propTypes = {
+  cap: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired
+};
 
 export default Header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header cap={452471038865} />
         <Coin name="Bitcoin" acronym="BTC" value={11116} cap={188698026949} />
         <Coin name="Etherum" acronym="ETH" value={842} cap={82581287147} />
         <Coin name="NEO" acronym="NEO" value={117} cap={7624760000} />
