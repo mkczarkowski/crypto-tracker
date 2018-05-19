c0a8e0dfa8c4c2b5e07e6765b507b615df62e298
mkczarkowski <marcinczarkowski@onet.eu>
2018-05-19 10:00:13 +0200

    fix: remove spaces from decimal part of currency value

--- src/shared/utils/helpers.js
+++ src/shared/utils/helpers.js
@@ -2,14 +2,16 @@ export default function formatAsCurrency(value) {
   const currencyFormatRegex = /\B(?=(\d{3})+(?!\d))/g;
   const separator = '.';
   const [wholePart, decimalPart] = String(value).split(separator);
+
   const wholePartFormattedAsCurrency = wholePart.replace(
     currencyFormatRegex,
     ' ',
   );
-  const formattedValue = decimalPart
-    ? [wholePartFormattedAsCurrency, decimalPart].join(separator)
-    : wholePartFormattedAsCurrency;
 
-  return `$${formattedValue}
-    `;
+  const formattedValue =
+    typeof decimalPart !== 'undefined'
+      ? [wholePartFormattedAsCurrency, decimalPart].join(separator)
+      : wholePartFormattedAsCurrency;
+
+  return `$${formattedValue}`;
 }
