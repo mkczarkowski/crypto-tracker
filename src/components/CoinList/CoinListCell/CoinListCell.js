19434bb45ddc4cbbfd5d7bdc38769191e41a8bff
mkczarkowski <marcinczarkowski@onet.eu>
2018-08-05 08:37:19 +0200

    refactor: import styled components from separate files

--- src/components/CoinList/CoinListCell/CoinListCell.js
+++ src/components/CoinList/CoinListCell/CoinListCell.js
@@ -1,28 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
-const cellPadding = 10;
-const largeCellFlex = '1 0 25%';
-const smallCellFlex = '1 0 10%';
+import { StyledWrapper } from './styled/Wrapper';
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex: ${props => (props.isLarge ? largeCellFlex : smallCellFlex)};
-  font-weight: ${props => (props.isHeader ? 700 : 300)};
-  padding-top: ${cellPadding}px;
-  padding-bottom: ${cellPadding}px;
-  border-bottom: ${props => (props.isHeader ? 4 : 2)}px solid #dedede;
-`;
-
-const CoinListCell = ({ isLarge, isHeader, children }) => {
-  return (
-    <Wrapper isLarge={isLarge} isHeader={isHeader}>
-      {children}
-    </Wrapper>
-  );
-};
+const CoinListCell = ({ isLarge, isHeader, children }) => (
+  <StyledWrapper isLarge={isLarge} isHeader={isHeader}>
+    {children}
+  </StyledWrapper>
+);
 
 CoinListCell.propTypes = {
   isLarge: PropTypes.bool,
--- src/components/CoinList/CoinListRow/CoinListRow.js
+++ src/components/CoinList/CoinListRow/CoinListRow.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-import styleVars from '../../../shared/styles/variables';
+import { StyledWrapper } from './styled/Wrapper';
 
 const Wrapper = styled.div`
   display: flex;
